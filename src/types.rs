576b7b5b3ace12fc7a4512912da1cddc5e6ca98b
Chris Elsworth <chris@cae.me.uk>
2021-06-08 19:34:12 +0100

    Add missed types.rs

---
+++ src/types.rs
@@ -0,0 +1,44 @@
+use crate::prelude::*;
+
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Serial([u8; 10]);
+
+impl Serial {
+    pub fn new(input: &[u8]) -> Self {
+        let mut r: [u8; 10] = Default::default();
+        r.copy_from_slice(input);
+        Self(r)
+    }
+
+    pub fn data(&self) -> [u8; 10] {
+        self.0
+    }
+}
+
+impl std::str::FromStr for Serial {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 10 {
+            return Err(anyhow!("{} must be exactly 10 characters", s));
+        }
+
+        let mut r: [u8; 10] = Default::default();
+        r.copy_from_slice(s.as_bytes());
+        Ok(Self(r))
+    }
+}
+
+impl fmt::Display for Serial {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
+    }
+}
+
+impl fmt::Debug for Serial {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
+    }
+}
