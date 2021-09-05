5d2f511ce4b9974e444c7ce2bda7eba1db1b6005
Chris Elsworth <chris@cae.me.uk>
2021-09-05 23:05:23 +0100

    add unique_id

--- src/home_assistant.rs
+++ src/home_assistant.rs
@@ -10,17 +10,19 @@ pub struct Config {
     state_class: String,    // measurement or total_increasing (energy)
     value_template: String, // value_json.p_pv or whatever
     unit_of_measurement: String,
+    unique_id: String,
 }
 
 impl Config {
     pub fn p_pv(inverter: &config::Inverter) -> Result<mqtt::Message> {
         let p_pv = Self {
             device_class: "power".to_owned(),
-            name: "p_pv".to_owned(),
+            name: format!("{} p_pv", inverter.datalog),
             state_topic: format!("lxp/{}/inputs/1", inverter.datalog),
             state_class: "measurement".to_owned(),
             value_template: "{{ value_json.p_pv }}".to_owned(),
             unit_of_measurement: "W".to_owned(),
+            unique_id: format!("{}_p_pv", inverter.datalog),
         };
 
         Ok(mqtt::Message {
