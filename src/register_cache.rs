a509a68694098c6aa8fd74eabab5b3ff96d97e0f
Chris Elsworth <chris@cae.me.uk>
2024-03-13 16:46:28 +0000

    Improve RegisterCache to support Hold/Input registers
    And write all incoming data to it

--- src/register_cache.rs
+++ src/register_cache.rs
@@ -5,23 +5,32 @@ const REGISTER_COUNT: usize = 256;
 
 #[derive(Clone, Debug)]
 pub enum ChannelData {
-    ReadRegister(u16, Rc<RefCell<oneshot::Sender<u16>>>),
-    RegisterData(u16, u16),
+    ReadRegister(Register, Rc<RefCell<oneshot::Sender<u16>>>),
+    RegisterData(Register, u16),
     Shutdown,
+}
+
+#[derive(Clone, Debug)]
+pub enum Register {
+    Hold(u16),
+    Input(u16),
 }
 
 pub struct RegisterCache {
     channels: Channels,
-    register_data: Rc<RefCell<[u16; REGISTER_COUNT]>>,
+    hold_register_data: Rc<RefCell<[u16; REGISTER_COUNT]>>,
+    input_register_data: Rc<RefCell<[u16; REGISTER_COUNT]>>,
 }
 
 impl RegisterCache {
     pub fn new(channels: Channels) -> Self {
-        let register_data = Rc::new(RefCell::new([0; REGISTER_COUNT]));
+        let hold_register_data = Rc::new(RefCell::new([0; REGISTER_COUNT]));
+        let input_register_data = Rc::new(RefCell::new([0; REGISTER_COUNT]));
 
         Self {
             channels,
-            register_data,
+            hold_register_data,
+            input_register_data,
         }
     }
 
@@ -35,8 +44,9 @@ impl RegisterCache {
     //
     //   RegisterCache::get(&self.channels, 1);
     //
-    pub async fn get(channels: &Channels, register: u16) -> u16 {
+    pub async fn get(channels: &Channels, register: Register) -> u16 {
         let (tx, rx) = oneshot::channel();
+
         let channel_data = ChannelData::ReadRegister(register, Rc::new(RefCell::new(tx)));
         let _ = channels.read_register_cache.send(channel_data);
         rx.await
@@ -49,18 +59,30 @@ impl RegisterCache {
         info!("register_cache getter starting");
 
         while let ChannelData::ReadRegister(register, reply_tx) = receiver.recv().await? {
-            if register < REGISTER_COUNT as u16 {
-                let register_data = self.register_data.borrow();
-                let value = register_data[register as usize];
+            match register {
+                Register::Hold(r) => {
+                    if r < REGISTER_COUNT as u16 {
+                        let register_data = self.hold_register_data.borrow();
+                        let value = register_data[r as usize];
 
-                let reply_tx = Rc::try_unwrap(reply_tx).unwrap();
-                let _ = reply_tx.into_inner().send(value);
-            } else {
-                warn!(
-                    "cannot cache register {}, increase REGISTER_COUNT!",
-                    register
-                );
-            }
+                        let reply_tx = Rc::try_unwrap(reply_tx).unwrap();
+                        let _ = reply_tx.into_inner().send(value);
+                    } else {
+                        bail!("cannot cache hold register {}, increase REGISTER_COUNT!", r);
+                    }
+                }
+                Register::Input(r) => {
+                    if r < REGISTER_COUNT as u16 {
+                        let register_data = self.input_register_data.borrow();
+                        let value = register_data[r as usize];
+
+                        let reply_tx = Rc::try_unwrap(reply_tx).unwrap();
+                        let _ = reply_tx.into_inner().send(value);
+                    } else {
+                        bail!("cannot cache input register {}, increase REGISTER_COUNT!", r);
+                    }
+                }
+            };
         }
 
         info!("register_cache getter exiting");
@@ -74,15 +96,25 @@ impl RegisterCache {
         info!("register_cache setter starting");
 
         while let ChannelData::RegisterData(register, value) = receiver.recv().await? {
-            if register < REGISTER_COUNT as u16 {
-                let mut register_data = self.register_data.borrow_mut();
-                register_data[register as usize] = value;
-            } else {
-                warn!(
-                    "cannot cache register {}, increase REGISTER_COUNT!",
-                    register
-                );
-            }
+            // debug!("register_cache setting {:?}={}", register, value);
+            match register {
+                Register::Hold(r) => {
+                    if r < REGISTER_COUNT as u16 {
+                        let mut register_data = self.hold_register_data.borrow_mut();
+                        register_data[r as usize] = value;
+                    } else {
+                        bail!("cannot cache hold register {}, increase REGISTER_COUNT!", r);
+                    }
+                }
+                Register::Input(r) => {
+                    if r < REGISTER_COUNT as u16 {
+                        let mut register_data = self.input_register_data.borrow_mut();
+                        register_data[r as usize] = value;
+                    } else {
+                        bail!("cannot cache input register {}, increase REGISTER_COUNT!", r);
+                    }
+                }
+            };
         }
 
         info!("register_cache setter exiting");
